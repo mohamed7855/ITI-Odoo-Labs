07597ac923a244a987127b498fc162e73c579376
mohamed7855 <martm7726@gmail.com>
2024-02-12 15:17:02 +0200

    make group for user&manager and put authorization

--- custom_addons/hms/__manifest__.py
+++ custom_addons/hms/__manifest__.py
@@ -3,6 +3,7 @@
     'description': 'student courses',
     'author': 'omara',
     'depends': ['base'],
-    'data': ['views/patient.xml', 'views/doctor.xml', 'views/department.xml', 'views/customer.xml' ],
+    'data': ['views/patient.xml', 'views/doctor.xml', 'views/department.xml', 'views/customer.xml',
+             'views/res_groups.xml', 'security/ir.model.access.csv'],
     'website': 'https://www.google.com.eg/'
 }
